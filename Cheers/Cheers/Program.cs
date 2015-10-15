889fa4c08eacb974911c71c1bd3111feba173690
Lynn Sameulson <lynn.samuelson@gmail.com>
2015-10-15 14:28:12 -0500

    added methods to the program

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -10,13 +10,46 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
-           System.Console.WriteLine("Hello There! What's your name?");
-           string name = System.Console.ReadLine();
-           System.Console.WriteLine("Hey " + name + ", What's your birthday? (MM/DD)");
-           string birthday = System.Console.ReadLine();
+            System.Console.WriteLine("Hello There! What's your name?");
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine("Hey " + name + ", What's your birthday? (MM/DD)");
+            string birthday = System.Console.ReadLine();
             System.Console.WriteLine("Hi, " + name);
-           name = name.ToUpper();
-           string choose = "a";
+            name = name.ToUpper();
+            nameCheer(name);
+            System.DateTime today = System.DateTime.Now;
+            System.DateTime birthdayDate = Convert.ToDateTime(birthday);
+            outputBirday(today, birthdayDate);
+
+            System.Console.WriteLine("Type any key to exit.");
+            System.Console.ReadKey();
+        }
+
+        private static void outputBirday(DateTime today, DateTime birthdayDate)
+        {
+            double days = (birthdayDate - today).TotalDays;
+            if (days < 0)
+            {
+                days = days + 365;
+            }
+            double numDays = Math.Round(days);
+            if (numDays == 0)
+            {
+                System.Console.WriteLine("Happy Birthday!");
+            }
+            else if (numDays == 1)
+            {
+                System.Console.WriteLine("Your Birthday is in " + numDays + " day.");
+            }
+            else
+            {
+                System.Console.WriteLine("Your Birthday is in " + numDays + " days.");
+            }
+        }
+
+        private static void nameCheer(string name)
+        {
+            string choose = "a";
             foreach (char letter in name.ToLower())
             {
                 if (Char.IsLetter(letter))
@@ -29,31 +62,9 @@ namespace Cheers
                         }
                     }
                     System.Console.WriteLine("Give me " + choose + "..." + letter);
-                }       
-            } 
-           System.Console.WriteLine(name + "'s just GRAND!");
-           System.DateTime today = System.DateTime.Now;
-           System.DateTime birthdayDate = Convert.ToDateTime(birthday);
-           double days = (birthdayDate - today).TotalDays;
-           if (days < 0)
-            {
-                days = days + 365;
+                }
             }
-           double numDays = Math.Round(days);
-           if (numDays == 0)
-            {
-                System.Console.WriteLine("Happy Birthday!");
-            } 
-           else if (numDays == 1)
-            {
-                System.Console.WriteLine("Your Birthday is in " + numDays + " day.");
-            }
-           else
-            {
-                System.Console.WriteLine("Your Birthday is in " + numDays + " days.");
-            }
-                                  
-           System.Console.ReadKey();
+            System.Console.WriteLine(name + "'s just GRAND!");
         }
     }
 }
